b5a1e4c552549b260ca5d99a6efb1ed5e9bf1292
Ran Navok <rnavok@gmail.com>
2017-04-12 15:53:20 +0300

    add POST todos route

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('./db/mongoose').mongoose;
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{    
+    
+    var newTodo = new Todo({
+        text: req.body.text
+    }).save().then((doc)=>{
+        console.log(`item added ${doc}`);
+        res.send(doc);        
+    },(err)=>{
+        console.log(`error while adding`,err);
+        res.status(400).send(err);
+    })
+});
+
+app.listen(3000 , ()=> {
+    console.log('server started');
+});
