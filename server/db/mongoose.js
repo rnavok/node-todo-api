ccb984169eb41e6f845b16bdbafa2e96b331388e
Ran Navok <rnavok@gmail.com>
2017-04-13 17:24:34 +0300

    mongo uri fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.Promise = global.Promise; 
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://test:test@ds161190.mlab.com:61190/node-todo-app');
+
 
 module.exports = {mongoose};
