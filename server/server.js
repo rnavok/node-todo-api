8343dea6760e843631e3c230c74511316d743543
Ran Navok <rnavok@gmail.com>
2017-04-14 11:35:16 +0300

    adding PATCH/todos:id to route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var ObjectID = require('mongodb').ObjectID;
+const _ = require ('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
 
 var mongoose = require('./db/mongoose').mongoose;
-
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -68,6 +68,32 @@ app.delete('/todos/:id',(req,res)=>{
     }).catch((e) =>{
         res.status(404).send();
     });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id =req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send('invalid id');
+
+        
+    var body = _.pick(req.body,['text','completed']);
+    console.log(body);
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();        
+    }else {
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new :true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();        
+        }
+        console.log(todo);
+        return res.send(todo);
+    }).catch((err) =>{
+          res.status(400).send();  
+    })    
 });
 
 app.listen(port , ()=> {
